3314f1dc726a524de65200b30a29f7d2f54ac4ba
Andrew Kim <andiskim@gmail.com>
2022-02-10 18:08:45 +0300

    Add notch in bottom modal

--- frontend/Components/Modal/BottomModal.js
+++ frontend/Components/Modal/BottomModal.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Modal from 'react-native-modal';
+import colors from '../../theme/colors';
 import { Box } from '../Box/Box';
 
 const BottomModal = (props) => {
@@ -23,6 +24,14 @@ const BottomModal = (props) => {
           borderTopLeftRadius={12}
           borderTopRightRadius={12}
         >
+          <Box
+            width='56px'
+            height='4px'
+            backgroundColor={colors.neutral_200}
+            borderRadius='4px'
+            mx='auto'
+            my='12px'
+          />
           {children}
         </Box>
       </Modal>
