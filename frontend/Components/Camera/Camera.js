b07b74ee03279b776f8f49f6cbf6676ba6888d40
Vasyl Martyniv <doc.people97@gmail.com>
2022-02-10 17:03:28 +0200

    Added react native camera scanner

--- frontend/Components/Camera/Camera.js
+++ frontend/Components/Camera/Camera.js
@@ -1,10 +1,46 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, {Component} from 'react';
 
-const Camera = () => {
-  return (
-    <Text>Camera</Text>
-  )
+import {StyleSheet, Linking} from 'react-native';
+
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import {RNCamera} from 'react-native-camera';
+
+class Camera extends Component {
+  onSuccess = e => {
+    Linking.openURL(e.data).catch(err =>
+      console.error('An error occured', err),
+    );
+  };
+
+  render() {
+    return (
+      <QRCodeScanner
+        onRead={this.onSuccess}
+        topContent={null}
+        bottomContent={null}
+      />
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
+  },
+});
 
 export default Camera;
